969ac78b546a77b978d65e660232d0722b7e43ee
Gabriel Bédard Sicé <bedardenator@gmail.com>
2014-02-22 16:00:57 -0500

    Made tree view a bit better

--- public/javascripts/views/tree-view.js
+++ public/javascripts/views/tree-view.js
@@ -1,12 +1,27 @@
 App.module('views', function(views, App, Backbone, Marionette, $, _) {
   var TreeView = views.TreeView = Marionette.CompositeView.extend({
-    template: _.template('<label><%= name %></label><ul class="list-group node-list"></ul>'),
+    template: _.template('<label><%= name %></label> \
+      <input type="text" value="<%= name %>"/> \
+      <ul class="list-group node-list"></ul> \
+    '),
     tagName: 'li',
     className: 'list-group-item',
     itemViewContainer: '.node-list',
 
     ui: {
-      nodeList: '.node-list'
+      nodeList: '.node-list',
+      label: 'label:first',
+      input: 'input:first'
+    },
+
+    events: {
+      'click label:first': 'onLabelClick',
+      'keypress input:first': 'onInputKeypress',
+      'blur input:first': 'onInputBlur'
+    },
+
+    modelEvents: {
+      'change:name': 'render'
     },
 
     initialize: function(options) {
@@ -15,6 +30,42 @@ App.module('views', function(views, App, Backbone, Marionette, $, _) {
 
     onRender: function() {
       if (!this.collection || !this.collection.length) this.ui.nodeList.hide();
+
+      if (this.model.has('name')) {
+        this.showLabel();
+      } else {
+        this.showInput();
+      }
+    },
+
+    onLabelClick: function() {
+      this.showInput();
+    },
+
+    showInput: function() {
+      this.ui.label.hide();
+      this.ui.input.show();
+      this.ui.input.focus();
+    },
+
+    showLabel: function() {
+      this.ui.label.show();
+      this.ui.input.hide();
+    },
+
+    onInputKeypress: function(e) {
+      if (e.keyCode === 13) {
+        this.onNameEdit();
+      }
+    },
+
+    onInputBlur: function() {
+      this.onNameEdit();
+    },
+
+    onNameEdit: function() {
+      this.model.set('name', this.ui.input.val());
+      this.showLabel();
     }
   });
 
